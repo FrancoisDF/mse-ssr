852e274aaa11f48b536c9c9429c636267b30f921
Francois Delpech <Francois.Delpech@schneider-electric.com>
2023-02-21 07:25:54 +0100

    not CSR

--- src/routes/icons/+page.ts
+++ src/routes/icons/+page.ts
@@ -1,1 +1,14 @@
-export const prerender = false;
+import { dev } from '$app/environment';
+
+// we don't need any JS on this page, though we'll load
+// it in dev so that we get hot module replacement
+export const csr = dev;
+
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
+
+// export const meta = { 
+//   title: 'overwrite tittle', 
+//   breadcrumb: true
+// }
