133001cfb3239b037e9b02f0cab417d50f136d44
Francois Delpech <Francois.Delpech@schneider-electric.com>
2023-03-13 10:44:46 +0100

    try without CSR

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,3 +10,16 @@ export const handle = (async ({ event, resolve }) => {
   });
   return response;
 }) satisfies Handle;
+
+// import type { Handle } from '@sveltejs/kit';
+
+
+// export const handle = (async ({ event, resolve }) => {
+//   const {renderToString} = require('https://d2osz8slymlqdp.cloudfront.net/@se/web-ui/@3.27.3/hydrate/index.js')
+//   const response = await resolve(event, {
+//     transformPageChunk: async ({ html }) => {
+//       return renderToString(html).then(results => results.html);
+//     },
+//   });
+//   return response;
+// }) satisfies Handle;
